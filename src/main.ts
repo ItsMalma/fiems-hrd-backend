076ca0699bfbc3916f57207a9b897742f8da8b09
ItsMalma <itsmalma28@gmail.com>
2023-07-07 22:50:36 +0700

    feat: create mongo connection
    create mongo connection by configuration.
    
    also, we move all the configuration fetch at the beginning.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,32 @@ import "reflect-metadata";
 import cors from "cors";
 import config from "config";
 import express from "express";
+import { MongoClient } from "mongodb";
 
 async function main() {
+  // get config
+  const mongoHost = config.has("mongo.host") ? config.get<string>("mongo.host") : "127.0.0.1";
+  const mongoPort = config.has("mongo.port") ? config.get<number>("mongo.port") : 5432;
+  const mongoDatabaseName = config.get<string>("mongo.database");
+  
+  const serverPort = config.get<number>("server.port") ?? 80;
+  
+  // create connection to mongo
+  const mongoUrl = `mongodb://${mongoHost}:${mongoPort}`;
+  const mongoClient = new MongoClient(mongoUrl);
+  await mongoClient.connect();
+  console.log(`Connected to ${mongoUrl}`);
+  
+  // get mongo database
+  const mongoDatabase = mongoClient.db(mongoDatabaseName);
+  
+  // close mongo connection when application exit
+  process.on("exit", async () => await mongoClient.close());
+  
+  // create express app
   const app = express();
+  
+  // register middleware to express app
   app.use(express.json());
   app.use(cors());
   
@@ -13,12 +36,9 @@ async function main() {
   
   // controller's routing
   
+  
   // run server
-  const port = config.get("server.port") as number ?? 80;
-  app.listen(
-    port, "localhost",
-    () => console.log(`Listening on :${port}`)
-  );
+  app.listen(serverPort, "localhost", () => console.log(`Listening on :${serverPort}`));
 }
 
 main()
