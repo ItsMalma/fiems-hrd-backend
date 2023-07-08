a1d364cca2d388d35e59a0fb593362de1fd94293
ItsMalma <itsmalma28@gmail.com>
2023-07-08 15:47:59 +0700

    feat: add employee response dto
    This can be solved by creating a separate class for each nested.

--- src/dtos/employee.dto.ts
+++ src/dtos/employee.dto.ts
@@ -276,3 +276,62 @@ export class AddEmployeeRequest {
   @ValidateNested({message: "must be object"})
   leave: AddEmployeeRequestLeave;
 }
+
+export class EmployeeResponse {
+  id: number;
+  detail: {
+    name: string
+    joining: Date
+    end: Date
+    nik: string
+    npwp: string
+    placeOfBirth: string
+    dateOfBirth: Date
+    gender: "Pria" | "Wanita"
+    bloodType: "A" | "B" | "AB" | "O"
+    religion: "Islam" | "Kristen" | "Hindu" | "Buddha" | "Kong Hu Chu" | "Lainnya"
+    latestEducation: "SD" | "SMP" | "SMA/SMK" | "Sarjana" | "Magister" | "Doktor"
+    yearOfGraduation: number
+  };
+  contactAndAddress: {
+    phoneNumber: string
+    email: string
+    emergencyContact: {
+      name: string
+      phoneNumber: string
+    }
+    address: string
+    kelurahan: string
+    kecamatan: string
+    city: string
+    province: string
+    zipCode: string
+  };
+  family: {
+    maritalStatus: "Single" | "Married" | "Divorce"
+    spouse: {
+      name: string
+      phoneNumber: string
+      address: string
+    }
+    mother: {
+      name: string
+      phoneNumber: string
+      address: string
+    }
+    father: {
+      name: string
+      phoneNumber: string
+      address: string
+    }
+    siblings: {
+      name: string
+      phoneNumber: string
+      address: string
+    }[]
+    tanggungan: unknown
+  };
+  leave: {
+    totalLeaves: number
+  };
+}
