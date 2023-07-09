e656cae6a3a4cce90cf99999509ce1f3280f27f4
ItsMalma <itsmalma28@gmail.com>
2023-07-09 08:52:55 +0700

    fix: fix migration for npwp index in employee

--- src/migration.ts
+++ src/migration.ts
@@ -23,9 +23,11 @@ async function migration() {
   const employeesCollection = await mongoDatabase.collection("employees");
   await employeesCollection.createIndexes([
     {name: "unique_nik_employee", key: {"detail.nik": 1}, unique: true},
-    {name: "unique_npwp_employee", key: {"detail.npwp": 1}, unique: true, sparse: true},
-    {name: "unique_phoneNumber_employee", key: {"detail.phoneNumber": 1}, unique: true},
-    {name: "unique_email_employee", key: {"detail.email": 1}, unique: true},
+    {name: "unique_npwp_employee", key: {"detail.npwp": 1}, unique: true, partialFilterExpression: {
+      detail: {npwp: {$ne: null}}
+    }},
+    {name: "unique_phoneNumber_employee", key: {"contactAndAddress.phoneNumber": 1}, unique: true},
+    {name: "unique_email_employee", key: {"contactAndAddress.email": 1}, unique: true},
   ]);
 }
 
