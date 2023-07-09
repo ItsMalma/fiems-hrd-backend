b73b54516ff4b5165b6f8ad43a66195757058974
ItsMalma <itsmalma28@gmail.com>
2023-07-09 08:54:28 +0700

    feat: add some transform method in employee mapper

--- src/mappers/employee.mapper.ts
+++ src/mappers/employee.mapper.ts
@@ -1,8 +1,19 @@
-import { AddEmployeeRequest, EmployeeResponse } from "@/dtos/employee.dto";
+import {
+  AddEmployeeRequest,
+  EmployeeResponse,
+  EmployeeShortResponse,
+  GetAllEmployeesRequest
+} from "@/dtos/employee.dto";
 import { Employee } from "@/entities/employee.entity";
+import { Sort } from "mongodb";
+import moment from "moment";
 
 export class EmployeeMapper {
   transformAddRequestIntoEntity(dto: AddEmployeeRequest): Employee {
+    dto.detail.joining = moment(dto.detail.joining, "DD/MM/YYYY", true).toDate();
+    dto.detail.end = dto.detail.end && moment(dto.detail.end, "DD/MM/YYYY", true).toDate();
+    dto.detail.dateOfBirth = moment(dto.detail.dateOfBirth, "DD/MM/YYYY", true).toDate();
+    
     return {
       _id: undefined,
       detail: dto.detail,
@@ -13,6 +24,21 @@ export class EmployeeMapper {
     };
   }
   
+  transformGetAllRequestIntoSort(dto: GetAllEmployeesRequest): Sort {
+    const sort: Sort = {};
+    
+    switch (dto.sort) {
+    case "az":
+      sort["detail.name"] = 1;
+      break;
+    case "za":
+      sort["detail.name"] = -1;
+      break;
+    }
+    
+    return sort;
+  }
+  
   transformEntityToResponse(entity: Employee): EmployeeResponse {
     return {
       id: entity._id,
@@ -21,5 +47,20 @@ export class EmployeeMapper {
       family: entity.family,
       leave: entity.leave
     };
+  }
+  
+  transformEntityToShortResponse(entity: Employee): EmployeeShortResponse {
+    return {
+      id: entity._id,
+      name: entity.detail.name
+    };
+  }
+  
+  transformEntitiesToResponses(entities: Employee[]): EmployeeResponse[] {
+    return entities.map(entity => this.transformEntityToResponse(entity));
+  }
+  
+  transformEntitiesToShortResponses(entities: Employee[]): EmployeeShortResponse[] {
+    return entities.map(entity => this.transformEntityToShortResponse(entity));
   }
 }
